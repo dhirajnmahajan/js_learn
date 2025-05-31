616ac7bb4ea9854902fd6d712edec6398778d3c4
Dhiraj Mahajan <122169637+dhirajnmahajan@users.noreply.github.com>
2025-05-31 04:27:25 +0000

    variables_concept

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const account_Id = 122332
+let accountEmail = "abc@gmail.com"
+var pswd = "1234"
+accountCity = "Maharashtra"
+
+let accountState;
+// account_Id=123343
+
+// accountEmail = "xyz@gmail.com"= 
+// accountCity = "india"
+console.log(accountState);
+
+// console.table([account_Id,accountEmail,pswd,accountCity])
+
+
+
