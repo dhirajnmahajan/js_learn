6dae4a89a4c9a8b8ee55788f19fafae64a527898
Dhiraj Mahajan <122169637+dhirajnmahajan@users.noreply.github.com>
2025-05-31 05:16:10 +0000

    03_conversionOperation

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+// ** Primitive Data Types **
+
+// number 
+// boolean => true/false 
+// null => standalone value
+// string => " "
+// undefined => 
+// symbol => unique
+
+// ** Secondary/derived data type
+
+// object
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = true
+// console.log(typeof NaN)
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let randomNum =123
+let convertString = String (randomNum)
+// console.log( convertString)
+// console.log(typeof convertString)
+
+let boolNum = 0
+let isBoolean = Boolean (boolNum)
+// console.log(isBoolean)
+// console.log(typeof boolNum);
+
+
+
+
+
+
+
