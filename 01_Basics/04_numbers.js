0abb7ad98d7d182109fe3a646121470d26aa341c
Dhiraj Mahajan <122169637+dhirajnmahajan@users.noreply.github.com>
2025-06-03 05:37:39 +0000

    numbers & math

---
+++ 01_Basics/04_numbers.js
@@ -0,0 +1,11 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+let str = new String ("Dhiraj")
+
+console.log(str.charAt(2));
+console.log(str.indexOf('r'));
+console.log(str.length);
