aad5df4509efb9ba501409c133ab32a8da636175
Dhiraj Mahajan <122169637+dhirajnmahajan@users.noreply.github.com>
2025-06-10 04:37:44 +0000

    prject 3

--- 06_projects/02_project/p2.js
+++ 06_projects/02_project/p2.js
@@ -1,26 +1,21 @@
 console.log("dhiraj");
 
 const form = document.querySelector('form');
-const result = document.querySelector('#result');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-result.addEventListener('click', function(e){
-
-
 
 const height = parseInt(document.querySelector('#height').value);
-
 const weight = parseInt(document.querySelector('#weight').value);
-
+const result = document.querySelector('#result');
 
 
 if (height==='' || height<1 || isNaN(height)) {
     result.innerHTML = `enter a valid height ${height}`
 }
 else if (weight==='' || weight<1 || isNaN(weight)) {
-    result.innerHTML = `enter a valid height ${weight}`
+    result.innerHTML = `enter a valid weight ${weight}`
 }
 else{
     const bmi = (weight / ((height*height) / 10000)).toFixed(2);
@@ -29,6 +24,5 @@ else{
     result.innerHTML = `<span>${bmi}</span>`
 }
 
-})
 
 });
---
+++ 06_projects/03_project/p3.js
@@ -0,0 +1,6 @@
+const time = document.querySelector('#clock')
+
+setInterval(function(){
+    const date = new Date();
+    time.innerHTML = date.toLocaleTimeString();
+} ,1000);
